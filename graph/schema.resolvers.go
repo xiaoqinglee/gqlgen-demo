2b31270a5e48c95e0faf67bc53cfbbe8f56d5efa
还行吧 <XiaoqingLee@users.noreply.github.com>
2022-08-21 22:27:41 +0800

    implement resolver

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -8,16 +8,29 @@ import (
 	"fmt"
 	"gqlgen-demo/graph/generated"
 	"gqlgen-demo/graph/model"
+	"math/rand"
 )
 
 // CreateTodo is the resolver for the createTodo field.
+//func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+//	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+//}
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+	todo := &model.Todo{
+		Text: input.Text,
+		ID:   fmt.Sprintf("T%d", rand.Int()),
+		User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 // Todos is the resolver for the todos field.
+//func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+//	panic(fmt.Errorf("not implemented: Todos - todos"))
+//}
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+	return r.todos, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
